90c4578863d9b42db9e27875dbd4135b9e2cd6c7
Mikael Hallendal <micke@codefactory.se>
2003-02-19 08:14:23 +0000

    forgot to remove a header

--- dbus/dbus-server.c
+++ dbus/dbus-server.c
@@ -20,7 +20,7 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *
  */ 
-#include <stdlib.h>
+
 #include "dbus-server.h"
 #include "dbus-server-unix.h"
 #ifdef DBUS_BUILD_TESTS
