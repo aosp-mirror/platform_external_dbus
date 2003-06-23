f55377a708a404ab2c791421ff5f78121c7e50ac
Anders Carlsson <andersca@codefactory.se>
2003-06-23 17:39:48 +0000

    2003-06-23  Anders Carlsson  <andersca@codefactory.se>
    	* configure.in:
    	* gcj/.cvsignore:
    	* gcj/Hello.java:
    	* gcj/Makefile.am:
    	* gcj/TestMessage.java: (TestMessage), (TestMessage.main):
    	* gcj/org/.cvsignore:
    	* gcj/org/Makefile.am:
    	* gcj/org/freedesktop/.cvsignore:
    	* gcj/org/freedesktop/Makefile.am:
    	* gcj/org/freedesktop/dbus/.cvsignore:
    	* gcj/org/freedesktop/dbus/Makefile.am:
    	* gcj/org/freedesktop/dbus/Message.java: (Message),
    	(Message.Message):
    	* gcj/org/freedesktop/dbus/natMessage.cc:
    	Fix the build system.

---
+++ gcj/TestMessage.java
@@ -0,0 +1,8 @@
+import org.freedesktop.dbus.Message;
+
+public class TestMessage {
+    public static void main (String[] args) {
+	Message msg = new Message ("test", "bla");
+
+    }
+}
---
+++ gcj/org/freedesktop/dbus/natMessage.cc
@@ -0,0 +1,20 @@
+// This file was created by `gcjh -stubs'. -*- c++ -*-
+//
+// This file is intended to give you a head start on implementing native
+// methods using CNI.
+// Be aware: running `gcjh -stubs ' once more for this class may
+// overwrite any edits you have made to this file.
+
+#include <Message.h>
+#include <gcj/cni.h>
+#include <java/lang/UnsupportedOperationException.h>
+
+#include <dbus/dbus.h>
+
+::gnu::gcj::RawData *
+org::freedesktop::dbus::Message::dbus_message_new (::java::lang::String *, ::java::lang::String *)
+{
+  throw new ::java::lang::UnsupportedOperationException (JvNewStringLatin1 ("org::freedesktop::dbus::Message::dbus_message_new (::java::lang::String *, ::java::lang::String *) not implemented"));
+}
+
+
