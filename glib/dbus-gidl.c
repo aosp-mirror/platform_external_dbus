9a4c459076d118f2dd7eeb8dc81ad88c495b65bc
Colin Walters <walters@verbum.org>
2005-08-01 16:12:53 +0000

    2005-08-01  Colin Walters  <walters@verbum.org>
    	Patch from Joe Markus Clarke:
    
    	* glib/dbus-gidl.c (property_info_unref, arg_info_unref): Fix
    	use-after-free.

--- glib/dbus-gidl.c
+++ glib/dbus-gidl.c
@@ -662,8 +662,8 @@ property_info_unref (PropertyInfo *info)
   info->base.refcount -= 1;
   if (info->base.refcount == 0)
     {
-      base_info_free (info);
       g_free (info->type);
+      base_info_free (info);
     }
 }
 
@@ -722,8 +722,8 @@ arg_info_unref (ArgInfo *info)
   if (info->base.refcount == 0)
     {
       g_hash_table_destroy (info->annotations);
-      base_info_free (info);
       g_free (info->type);
+      base_info_free (info);
     }
 }
 
