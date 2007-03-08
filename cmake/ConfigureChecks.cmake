5f5a1a3366e2b30a2e2183222e3e5d704b9d5e58
Ralf Habacker <ralf.habacker@freenet.de>
2007-03-08 08:36:47 +0000

    * cmake/ConfigureChecks.cmake: fixed socketpair check, added setrlimit check really

--- cmake/ConfigureChecks.cmake
+++ cmake/ConfigureChecks.cmake
@@ -20,9 +20,10 @@ check_symbol_exists(getpeerucred "ucred.h"          HAVE_GETPEERUCRED)       #  
 check_symbol_exists(nanosleep    "time.h"           HAVE_NANOSLEEP)          #  dbus-sysdeps.c
 check_symbol_exists(getpwnam_r   "errno.h pwd.h"    HAVE_POSIX_GETPWNAM_R)   #  dbus-sysdeps-util-unix.c
 check_symbol_exists(setenv       "stdlib.h"         HAVE_SETENV)             #  dbus-sysdeps.c
-check_symbol_exists(socketpair   "sys/socket.h.h"   HAVE_SOCKETPAIR)         #  dbus-sysdeps.c
+check_symbol_exists(socketpair   "sys/socket.h"     HAVE_SOCKETPAIR)         #  dbus-sysdeps.c
 check_symbol_exists(unsetenv     "stdlib.h"         HAVE_UNSETENV)           #  dbus-sysdeps.c
 check_symbol_exists(writev       "sys/uio.h"        HAVE_WRITEV)             #  dbus-sysdeps.c, dbus-sysdeps-win.c
+check_symbol_exists(setrlimit    "sys/resource.h"   HAVE_SETRLIMIT)          #  dbus-sysdeps.c, dbus-sysdeps-win.c, test/test-segfault.c
 
 check_struct_member(cmsgcred cmcred_pid "sys/types.h sys/socket.h" HAVE_CMSGCRED)   #  dbus-sysdeps.c
 
