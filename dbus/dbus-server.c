c3793db07ff818af80498a3f78b03756977fce7b
Ralf Habacker <ralf.habacker@freenet.de>
2007-05-19 15:17:26 +0000

    * dbus/dbus-server.c (listen_funcs): _dbus_server_listen_platform_specific is empty on win32, limited to unix only. * dbus/dbus-server.c (valid_addresses): limit unix only addresses to unix.

--- dbus/dbus-server.c
+++ dbus/dbus-server.c
@@ -509,8 +509,10 @@ static const struct {
                                    DBusServer      **server_p,
                                    DBusError        *error);
 } listen_funcs[] = {
-  { _dbus_server_listen_socket },
-  { _dbus_server_listen_platform_specific }
+  { _dbus_server_listen_socket }
+#ifdef DBUS_UNIX
+  , { _dbus_server_listen_platform_specific }
+#endif
 #ifdef DBUS_BUILD_TESTS
   , { _dbus_server_listen_debug_pipe }
 #endif
@@ -1116,10 +1118,12 @@ _dbus_server_test (void)
 {
   const char *valid_addresses[] = {
     "tcp:port=1234",
-    "unix:path=./boogie",
     "tcp:host=localhost,port=1234",
     "tcp:host=localhost,port=1234;tcp:port=5678",
+#ifdef DBUS_UNIX
+    "unix:path=./boogie",
     "tcp:port=1234;unix:path=./boogie",
+#endif
   };
 
   DBusServer *server;
