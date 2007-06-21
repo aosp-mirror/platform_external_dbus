62c638249ecc1a3ae3e4388fc9f4a45a80e34587
Ralf Habacker <ralf.habacker@freenet.de>
2007-06-21 21:58:36 +0000

    * dbus/dbus-tranport.c (auth_via_default_rules): made the verbose spam cross-platform

--- dbus/dbus-transport.c
+++ dbus/dbus-transport.c
@@ -619,21 +619,31 @@ auth_via_default_rules (DBusTransport *transport)
       _dbus_credentials_same_user (our_identity,
                                    auth_identity))
     {
-      /* FIXME the verbose spam here is unix-specific */                  
-      _dbus_verbose ("Client authorized as UID "DBUS_UID_FORMAT
-                     " matching our UID "DBUS_UID_FORMAT"\n",
-                     _dbus_credentials_get_unix_uid(auth_identity),
-                     _dbus_credentials_get_unix_uid(our_identity));
+      if (_dbus_credentials_include(our_identity,DBUS_CREDENTIAL_WINDOWS_SID))
+          _dbus_verbose ("Client authorized as SID '%s'"
+                         "matching our SID '%s'\n",
+                         _dbus_credentials_get_windows_sid(auth_identity),
+                         _dbus_credentials_get_windows_sid(our_identity));
+      else
+          _dbus_verbose ("Client authorized as UID "DBUS_UID_FORMAT
+                         " matching our UID "DBUS_UID_FORMAT"\n",
+                         _dbus_credentials_get_unix_uid(auth_identity),
+                         _dbus_credentials_get_unix_uid(our_identity));
       /* We have authenticated! */
       allow = TRUE;
     }
   else
     {
-      /* FIXME the verbose spam here is unix-specific */
-      _dbus_verbose ("Client authorized as UID "DBUS_UID_FORMAT
-                     " but our UID is "DBUS_UID_FORMAT", disconnecting\n",
-                     _dbus_credentials_get_unix_uid(our_identity),
-                     _dbus_credentials_get_unix_uid(our_identity));
+      if (_dbus_credentials_include(our_identity,DBUS_CREDENTIAL_WINDOWS_SID))
+          _dbus_verbose ("Client authorized as SID '%s'"
+                         " but our SID is '%s', disconnecting\n",
+                         _dbus_credentials_get_windows_sid(our_identity),
+                         _dbus_credentials_get_windows_sid(our_identity));
+      else
+          _dbus_verbose ("Client authorized as UID "DBUS_UID_FORMAT
+                         " but our UID is "DBUS_UID_FORMAT", disconnecting\n",
+                         _dbus_credentials_get_unix_uid(our_identity),
+                         _dbus_credentials_get_unix_uid(our_identity));
       _dbus_transport_disconnect (transport);
       allow = FALSE;
     }  
