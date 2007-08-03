d9f2438806d0b2584003a3c3a54234c5a1fac4c2
Havoc Pennington <hp@redhat.com>
2007-08-03 19:05:29 +0000

    2007-08-03  Havoc Pennington  <hp@redhat.com>
    	* configure.in: add major/minor/micro version number AC_SUBST
    
    	* dbus/dbus-arch-deps.h.in (DBUS_MAJOR_VERSION,
    	DBUS_MINOR_VERSION, DBUS_MICRO_VERSION, DBUS_VERSION_STRING,
    	DBUS_VERSION): collection of macros to get version of library we
    	are compiled against.
    
    	* dbus/dbus-misc.c (dbus_get_version): new function, to get
    	version of library we are linked against at runtime.

--- dbus/dbus-misc.h
+++ dbus/dbus-misc.h
@@ -37,7 +37,11 @@ DBUS_BEGIN_DECLS
  * @{
  */
 
-char*       dbus_get_local_machine_id              (void);
+char*       dbus_get_local_machine_id  (void);
+
+void        dbus_get_version           (int *major_version_p,
+                                        int *minor_version_p,
+                                        int *micro_version_p);
 
 /** @} */
 
