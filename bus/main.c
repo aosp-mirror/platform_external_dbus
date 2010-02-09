523e01827eac49ec30afa173d31cdeee4be6381a
Ralf Habacker <ralf.habacker@freenet.de>
2010-02-09 08:37:50 +0100

    fixed bug on win32 not been able to use session bus with --config-file option
    Internal setup of session bus type was bound to the presence of the --session
    command line parameter which prevents to use the --config-file parameter for
    session bus setup.

--- bus/main.c
+++ bus/main.c
@@ -298,7 +298,6 @@ main (int argc, char **argv)
         }
       else if (strcmp (arg, "--session") == 0)
         {
-          is_session_bus = TRUE;
           check_two_config_files (&config_file, "session");
 
           if (!_dbus_append_session_config_file (&config_file))
@@ -454,6 +453,9 @@ main (int argc, char **argv)
       dbus_error_free (&error);
       exit (1);
     }
+
+  is_session_bus = bus_context_get_type(context) != NULL
+      && strcmp(bus_context_get_type(context),"session") == 0;
 
   if (is_session_bus)
     _dbus_daemon_publish_session_bus_address (bus_context_get_address (context));
