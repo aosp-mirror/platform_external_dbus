03bb3ce656e9b14fe643572dd0d39f82e955f1f5
Colin Walters <walters@verbum.org>
2010-03-22 10:38:12 -0400

    Add DBUS_GNUC_PRINTF checks to new formatting functions
    Otherwise we don't get GCC warnings.

--- bus/bus.c
+++ bus/bus.c
@@ -1176,6 +1176,9 @@ bus_context_get_reply_timeout (BusContext *context)
 }
 
 void
+bus_context_log (BusContext *context, DBusSystemLogSeverity severity, const char *msg, ...) _DBUS_GNUC_PRINTF (3, 4);
+
+void
 bus_context_log (BusContext *context, DBusSystemLogSeverity severity, const char *msg, ...)
 {
   va_list args;
