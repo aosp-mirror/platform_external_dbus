6881a39384a9dea44aacbc315245c283f6ac75ca
Doug Kwan <dougkwan@google.com>
2011-06-26 12:08:40 -0700

    Suppress gcc warning to make dbus compile using gcc-4.6.  This is done also by upstream in dbus-1.4.6.  See:
    Change-Id: I01c1b54980896849c855e457456e95a69afe54e9
    http://lists.freedesktop.org/archives/dbus/2011-February/014122.html

--- bus/Android.mk
+++ bus/Android.mk
@@ -13,6 +13,10 @@ LOCAL_CFLAGS+=-DDBUS_DAEMON_NAME=\"dbus-daemon\"
 LOCAL_CFLAGS+=-DDBUS_SYSTEM_CONFIG_FILE=\"/system/etc/dbus.conf\"
 LOCAL_CFLAGS+=-DDBUS_SESSION_CONFIG_FILE=\"/system/etc/session.conf\"
 
+# We get warning in the _DBUS_ASSERT_ERROR_IS_SET macro.  Suppress
+# this warning so that we can compile with Werror.  The warning
+# is also ignored in dbus-1.4.6.
+LOCAL_CFLAGS+=-Wno-address
 
 LOCAL_SRC_FILES:= \
 	activation.c \
